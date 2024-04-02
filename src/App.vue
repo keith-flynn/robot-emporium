86b639c91b7747f78bd71f2daabf95c9dac7752a
Keith Flynn <flynn.keith@gmail.com>
2024-04-02 16:39:32 -0400

    Add routing
    Home page has fixed link, all others still need updated.

--- src/App.vue
+++ src/App.vue
@@ -11,21 +11,13 @@
   </header>
   <main>
     <!-- <HomePage msg="Welcome to Your Vue.js App"/> -->
-    <RobotBuilder />
+    <router-view />
   </main>
 </template>
 
-<script>
-// import HomePage from './home/HomePage.vue';
-import RobotBuilder from './build/RobotBuilder.vue';
-// import ProductSearch from './search/ProductSearch.vue';
+<script setup>
+// import { ref, provide } from 'vue';
 
-export default {
-  name: 'App',
-  components: {
-    RobotBuilder,
-  },
-};
 </script>
 
 <style>
